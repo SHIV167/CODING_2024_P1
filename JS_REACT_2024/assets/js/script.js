71016693d4a1588762799573bf4a35c0007ca667
SHIV167 <jhashiv5@gmail.com>
2024-07-02 16:49:35 +0530

    new script added

--- JS_REACT_2024/assets/js/script.js
+++ JS_REACT_2024/assets/js/script.js
@@ -3,7 +3,7 @@ let Main = document.getElementsByTagName("main")[0];
 let Click = document.getElementById("me");
 async function Getdata(Topic) {
   let Data = await fetch(
-    `https://newsapi.org/v2/everything?q=${Topic}&from=2024-06-01&sortBy=publishedAt&apiKey=d505d9b0bdef4983a254fd1a6206dd92`
+    `https://newsapi.org/v2/everything?q=${Topic}&from=2024-06-02&sortBy=publishedAt&apiKey=d505d9b0bdef4983a254fd1a6206dd92`
   );
   let Res = await Data.json();
   let Finaldata = Res.articles;
@@ -31,3 +31,17 @@ function Search() {
   let Tab = me.value;
   Getdata(Tab);
 }
+//
+let Start = "";
+let c = 1;
+for (let i = 0; i <= 10; i++) {
+  for (let j = 0; j <= i; j++) {
+    Start += c++ + "";
+  }
+  Start += "\n";
+}
+console.log(Start);
+let Arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+Arr.slice(2, 4);
+//Arr.splice(3, 1, 11, 12, 13);
+console.log(Arr);
